e9d0bd2d01793a1b430b2712d568786899e65f55
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-01 10:44:03 -0300

    Create main

---
+++ src/main_bitwise.c
@@ -0,0 +1,30 @@
+/*
+ *
+ * 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include "bitwise.h"
+
+
+int
+main(int argc, char **argv){
+
+    int c, *longindex = 0;
+    const char *optstring = "u:l"
+
+    while ((c = getopt_long(argc, argv, optstring, long_option, &optind)) != -1){
+        switch (expression)
+        {
+        case 'u':
+            toupper();
+            break;
+        case 'l':
+            break
+        case 'h':
+            usage(EXIT_SUCCESS);
+        default:
+            usage(EXIT_FAILURE);
+        }
+    }
+};
