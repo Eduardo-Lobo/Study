fc9bbb7204afc7ca0d7f2bc57b60fb1203984ad6
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-01 16:48:40 -0300

    Create upper_f func

--- src/bitwise.c
+++ src/bitwise.c
@@ -27,6 +27,17 @@ usage(int status){
 };
 
 
+char 
+*upper_f(unsigned char *word){
+    
+    const int x = 32;
+
+    if (word[0] = word[0] & ~x){
+        return word;
+    }
+};
+
+
 char
 *toupper_f(unsigned char *word){
 
