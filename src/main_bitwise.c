78585cfcb355528f2e48184b094d02bc1c4342e0
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-01 16:25:58 -0300

    Add long_option and rename to longopts

--- src/main_bitwise.c
+++ src/main_bitwise.c
@@ -4,7 +4,16 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 #include "bitwise.h"
+
+const struct option longopts[]=
+{
+  {"toupper", required_argument, NULL, 'u'},
+  {"tolower", required_argument, NULL, 'l'},
+  {"help",    no_argument,       NULL, 'h'},
+  {0,         0,                 0,      0},
+};
 
 
 int
@@ -13,18 +22,16 @@ main(int argc, char **argv){
     int c, *longindex = 0;
     const char *optstring = "u:l";
 
-    while ((c = getopt_long(argc, argv, optstring, long_option, &optind)) != -1){
-        switch (expression)
+    while ((c = getopt_long(argc, argv, optstring, longopts, &optind)) != -1){
+        switch (c)
         {
         case 'u':
-            toupper_f();
+            printf("%s\n", toupper_f(argv[2]));
             break;
         case 'l':
             break;
         case 'h':
             usage(EXIT_SUCCESS);
-        default:
-            usage(EXIT_FAILURE);
         }
     }
 };
