47fc15b9f5f6adee09e347fbff22ace7270284d5
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-01 22:42:22 -0300

    Create lower_f func

--- src/bitwise.c
+++ src/bitwise.c
@@ -30,6 +30,17 @@ usage(int status){
 };
 
 
+char
+*lower_f(unsigned char *word){
+
+    const int x = 32;
+
+    if (word[0] = word[0] | x){
+        return word;
+    }
+}
+
+
 char 
 *tolower_f(unsigned char *word){
 
