c4b48a33be34b7e29c6c6ff06f889002d45591b6
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-02 00:02:15 -0300

    Add description for declares

--- src/bitwise.h
+++ src/bitwise.h
@@ -3,9 +3,13 @@
  *
  */
 #include <stdlib.h>
-
+/* Invert word for uppercase                           */
 extern char *toupper_f(unsigned char *word );
-extern char *tolower_f(unsigned char *word );
+/* Invert first char for uppercase                     */
 extern char *upper_f(unsigned char *word);
+/* Invert word for lowercase                           */
+extern char *tolower_f(unsigned char *word );
+/* Invert first char for lowercase                     */
 extern char *lower_f(unsigned char *word);
+/* Info usage program                                  */
 extern void usage(int status);
