786eecef33043fc7012af03d23f8c0e855e83821
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-04 09:20:18 -0300

    Create extract_day and extract_year func

--- src/date.c
+++ src/date.c
@@ -26,13 +26,22 @@ todate_f(int opt, int month, int day, int year){
     if (month < 1 || month > 12 || day < 1 || day > 31 || year < 00 || year > 99){
         usage(EXIT_FAILURE);
     }
-    else if (opt == 'd')
-    {
+    else if (opt == 'D'){
+        
         printf("Date: %d/%d/%d Byte: %zu\n", 
         d.month, d.day, d.year, sizeof d);
     }
-    else if (opt == 'e'){
+    else if (opt == 'm'){
+        
         extract_month(d.month);
+    }
+    else if (opt == 'd'){
+        
+        extract_day(d.day);
+    }
+    else if (opt == 'y'){
+
+        extract_year(d.year);
     }
 };
 
@@ -41,4 +50,14 @@ void
 extract_month(int month){
     
     printf("extract_month: %d\n", month);
+};
+
+void 
+extract_day(int day){
+    printf("extract_day: %d\n", day);
+};
+
+void 
+extract_year(int year){
+    printf("extract_year: %d\n", year);
 };
