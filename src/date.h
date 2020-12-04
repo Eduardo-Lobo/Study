20ac8253141cdc29875ebd2e9113246b86f3bdc3
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-04 08:35:21 -0300

    Declare extract_month

--- src/date.h
+++ src/date.h
@@ -1,8 +1,9 @@
 
 
 
-extern int todate_f(int month, int day, int year);
+extern int todate_f(int opt, int month, int day, int year);
 extern void enter_dt(void);
+extern void extract_month(int month);
 struct Date
 {
     unsigned int month : 4, day : 5, year : 7;
