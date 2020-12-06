04be650cfe3daa4b86ecc172654a1b91a51e0d2d
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-06 14:58:42 -0300

    Declare test_bit and check

--- src/cntbits.h
+++ src/cntbits.h
@@ -4,7 +4,27 @@
  * 
  */ 
 
-extern void cnt_bits(unsigned int number);
+/* 
+Counts the number of one bits in a 16-bit integer value.
+*/
+extern int cnt_bits(unsigned int number);
+/* 
+Check if n have a range 0..15.
+The function should always return 1 if n holds a 
+value outside the range 0..15, otherwise 0. 
+*/
+extern unsigned int check(unsigned int n);
+/* 
+This function requires two 16-bit integer parameters. 
+The first parameter is a 16-bit value to test; 
+the second parameter is a value in the range 0..15
+describing which bit to test.
+
+The function should return <b>true</> if the corresponding bit 
+contains a 1, the function should return <b>false</b> if that 
+bit position contains a 0.
+*/
+extern unsigned int test_bit(unsigned int n, unsigned int p);
 
 #define biterr "N %d is not 16 bits\n"
 #define bitscc "N of bits: %d\n"
