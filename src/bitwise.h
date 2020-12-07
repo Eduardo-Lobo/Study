3369346ed7eef175bee8df0dd578a1ac463818f6
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-07 16:15:57 -0300

    Declare rol and ror

--- src/bitwise.h
+++ src/bitwise.h
@@ -3,12 +3,32 @@
  *
  * 
  */
-#include <stdlib.h>
-/* Invert word for uppercase                           */
+#include <stdint.h>
+/*
+Take a single word as their parameter and
+convert this word to uppercase.
+*/
 extern char *toupper_f(unsigned char *word );
-/* Invert first char for uppercase                     */
+/* 
+Take a single word as their parameter and
+convert the first character to uppercase
+*/
 extern char *upper_f(unsigned char *word);
-/* Invert word for lowercase                           */
+/*
+Take a single word as their parameter and
+convert this word to lowercase.
+*/
 extern char *tolower_f(unsigned char *word );
-/* Invert first char for lowercase                     */
+/* 
+Take a single word as their parameter and
+convert the first character to lowercase.
+*/
 extern char *lower_f(unsigned char *word);
+/* 
+
+*/
+extern uint32_t rol(uint32_t x, uint32_t n);
+/* 
+    
+*/
+extern uint32_t ror(uint32_t x, uint32_t n);
