a1302bdda8f95cac329a7c0f2271e9d948f1ce05
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-07 16:17:25 -0300

    Create rol and ror func

--- src/bitwise.c
+++ src/bitwise.c
@@ -2,30 +2,34 @@
  *
  * 
  */
+
 /*                */
 #include <stdint.h>
+#include <assert.h>
 #include <stdio.h>
 /*                */
 #include "bitwise.h"
 
 
 char
-*lower_f(unsigned char *word){
-
+*lower_f(unsigned char *word)
+{
     const int x = 32;
 
-    if (word[0] = word[0] | x){
+    if (word[0] = word[0] | x)
+    {
         return word;
     }
 };
 
 
 char 
-*tolower_f(unsigned char *word){
-
+*tolower_f(unsigned char *word)
+{
     const int x = 32;
 
-    for (int i=0; word[i]!='\0'; i++){
+    for (int i=0; word[i]!='\0'; i++)
+    {
         word[i] = word[i] | x;
     }
     return word;
@@ -33,23 +37,41 @@ char
 
 
 char 
-*upper_f(unsigned char *word){
-    
+*upper_f(unsigned char *word)
+{
     const int x = 32;
 
-    if (word[0] = word[0] & ~x){
+    if (word[0] = word[0] & ~x)
+    {
         return word;
     }
 };
 
 
 char
-*toupper_f(unsigned char *word){
-
+*toupper_f(unsigned char *word)
+{
     const int x = 32;
 
-    for (int i=0; word[i]!='\0'; i++){
+    for (int i=0; word[i]!='\0'; i++)
+    {
         word[i] = word[i] & ~x;
     }
     return word;
+};
+
+
+uint32_t
+rol(uint32_t x, uint32_t n)
+{
+    assert (n < 32);
+    return (x << n) | (x >> (-n&31));
+};
+
+
+uint32_t
+ror(uint32_t x, uint32_t n)
+{
+    assert (n < 32);
+    return (x >> n) | (x << (-n&31));
 };
