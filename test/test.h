522253fa7caeaa08cf4f235d6df501a061629322
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-11 13:31:09 -0300

    Define lower, tolower, upper and toupper

--- test/test.h
+++ test/test.h
@@ -1,8 +1,24 @@
-
+/*                <b>To test bits.c</b<                                        */
+#define is_zero   "Number: %d in position %d have bit 0"
+#define is_one    "Number: %d in position %d have bit 1"
+#define is_16bit  "Number: %d\n is not a value 16-bit"
+#define is_n16bit "Number: %d\n is a value 16-bit"
+#define is_nrange "Number: %d is outside range 0..15"
+#define is_range  "Number: %d is within range 0..15"
 
 void test_cnt_bits(void);
 void test_test_bit(void);
 void test_check(void);
+
+/*               <b>To test bitwise.c<\b>                                   */ 
+#define lower "Expect that only first character convert to lowercase. \
+Word: %s result %s"
+#define tolower "Expect that all characters convert to lowercase. \
+Word: %s result %s"
+#define upper "Expect that only first character convert to uppercase. \
+Word: %s result %s"
+#define toupper "Expect that all characters convert to uppercase. \
+Word: %s result %s"
 
 void test_lower_f(void);
 void test_tolower_f(void);
