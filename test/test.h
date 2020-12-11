5a231180975fa708854bd87a788aa371ffdcb33d
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-11 12:46:12 -0300

    Declare test_lower_f, test_tolower_f, test_upper_f, test_toupper_f

--- test/test.h
+++ test/test.h
@@ -3,3 +3,8 @@
 void test_cnt_bits(void);
 void test_test_bit(void);
 void test_check(void);
+
+void test_lower_f(void);
+void test_tolower_f(void);
+void test_upper_f(void);
+void test_toupper_f(void);
