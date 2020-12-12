600247470c21e1fd4a783ad1e909c1665d37d4e5
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-12 15:13:59 -0300

    Add call to test_main_bits

--- test/test_main.c
+++ test/test_main.c
@@ -3,13 +3,5 @@
 
 void
 main(void){
-
-    test_cnt_bits();
-    test_test_bit();
-    test_check();
-    test_lower_f();
-    test_tolower_f();
-    test_upper_f();
-    test_toupper_f();
-
+    test_main_bits();
 };
