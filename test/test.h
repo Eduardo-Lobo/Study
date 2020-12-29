be6f61d2b70a4742b49fe58a8d1b66b4e1018369
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-29 13:04:08 -0300

    Remove definitions

--- test/test.h
+++ test/test.h
@@ -1,30 +1,18 @@
-/*                <b>To test bits.c</b<                                        */
-#define is_zero   "Number: %d in position %d have bit 0\n"
-#define is_one    "Number: %d in position %d have bit 1\n"
-#define larger_or_outside "Number: %d is not a 16-bit value or %d is outside range 0..15\n"
-#define is_n16bit "Number: %d is not a 16-bit value\n"
-#define is_16bit  "Number: %d is a 16-bit value\n"
-#define is_nrange "Number: %d is outside range 0..15\n"
-#define is_range  "Number: %d is within range 0..15\n"
-#define tobin     "Converting %d to binary:\n"
+#include <stdlib.h>
+#include <stdio.h>
 
-unsigned int test_cnt_bits(unsigned int n);
+/*  Test to src/bits.c     */
 unsigned int test_test_bit(unsigned int n, unsigned int p);
 unsigned int test_range(unsigned int n, unsigned int r);
+unsigned int test_cnt_bits(unsigned int n);
 unsigned int *test_tobin(unsigned int n);
 void test_main_bits(void);
 
-/*               <b>To test bitwise.c<\b>                                   */ 
-#define lower "Expect that only first character convert to lowercase. Word: LOWERCASE result %s\n"
-#define tolower "Expect that all characters convert to lowercase. Word: LOWERCASE result %s\n"
-#define upper "Expect that only first character convert to uppercase. Word: uppercase result %s\n"
-#define toupper "Expect that all characters convert to uppercase. Word: uppercase result %s\n"
+enum Bits {SIZE = 16, CBITS = 15};
 
-char *test_lower_f(char str[]);
-char *test_tolower_f(char str[]);
-char *test_upper_f(char str[]);
+/*  Test to src/bitwise.c  */
+unsigned int test_main_bitwise(void);
 char *test_toupper_f(char str[]);
-void test_main_bitwise(void);
-
-#define SIZE 16
-#define CBITS 15
+char *test_tolower_f(char str[]);
+char *test_lower_f(char str[]);
+char *test_upper_f(char str[]);
