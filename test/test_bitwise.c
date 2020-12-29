a510a79139c091217bbd29e831929f0964e6ef6e
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-29 13:13:35 -0300

    Modify test_main_bitwise func

--- test/test_bitwise.c
+++ test/test_bitwise.c
@@ -1,13 +1,13 @@
 /*
- * Create test to bitwise.c 
+ * Create: test to bitwise.c
+ * Author: Eduardo-Lobo @2020
+ * Email : myedudev@protonmail.ch
  */
- 
-/*                */ 
+
 #include <stdio.h>
 #include "test.h"
-/*                */
 
-
+/* Change the first character of str for lowercase. */
 char 
 *test_lower_f(char str[])
 {
@@ -19,7 +19,7 @@ char
     }
 };
 
-
+/* Change all characters of str for lowercase. */
 char 
 *test_tolower_f(char str[])
 {
@@ -33,7 +33,7 @@ char
 
 };
 
-
+/* Change the first character of str for uppercase. */
 char 
 *test_upper_f(char str[])
 {
@@ -46,6 +46,7 @@ char
     
 };
 
+/* Change all characters of str for uppercase. */
 char
 *test_toupper_f(char str[])
 {
@@ -59,23 +60,61 @@ char
 };
 
 
-void
+unsigned int
 test_main_bitwise(void){
 
-    char l[]="LOWERCASE", u[]="uppercase";
+    char str[] = "LoweRcASe";
+    const int x = 32;
+    char *ret_str;
+    int c = 0, i;
 
-    fputs("-------BEGINNING TEST TO BITWISE------\n\
-        \n-------<b>TEST TO LOWER_F</b>-------\n", stdout);
-    printf(lower, test_lower_f(l));
+    /* If the first character is uppercase at str, change for lowercase.  */
+    /* Character lowercase should continue lowercase. */
+    ret_str = test_lower_f(str);
+    
+    if (ret_str[0] == (str[0] | x)){
+        test_ret_bitwise("test_lower_f()", EXIT_SUCCESS);
+    }else{
+        test_ret_bitwise("test_lower_f()", EXIT_FAILURE);
+    }
 
-    fputs("\n-------<b>TEST TO TOLOWER_F</b>-------\n", stdout);
-    printf(tolower, test_tolower_f(l));
+    /* Check if all character uppercase at str have change for lowercase. */
+    /* Character lowercase should continue lowercase. */
+    ret_str = test_tolower_f(str);
+    
+    for (i = 0; str[i] != '\0'; i++){
+        if (ret_str[i] == (str[i] | x))
+            c++;
+    }
+    if (c == sizeof(str) -1){
+        test_ret_bitwise("test_tolower_f()", EXIT_SUCCESS);
+    }else{ 
+        test_ret_bitwise("test_tolower_f()", EXIT_FAILURE);
+    }
 
-    fputs("\n-------<b>TEST TO UPPER_F</b>-------\n", stdout);
-    printf(upper, test_upper_f(u));
+    /* If the first character is lowercase at str, change for uppercase.  */
+    /* Character uppercase should continue uppercase. */
+    ret_str = test_upper_f(str);
 
-    fputs("\n-------<b>TEST TO TOUPPER_F</b>-------\n", stdout);
-    printf(toupper, test_toupper_f(u));
+    if (ret_str[0] == (str[0] & ~x)){
+        test_ret_bitwise("test_upper_f()", EXIT_SUCCESS);
+    }else{
+        test_ret_bitwise("test_upper_f()", EXIT_FAILURE);
+    }
 
-    fputs("\n-------END TEST BITWISE-------\n", stdout);
+
+    /* Check if all character lowercase at str have change for uppercase. */
+    /* Character uppercase should continue uppercase. */
+    ret_str = test_toupper_f(str);
+    c = 0;
+
+    for (i = 0; str[i] != '\0'; i++){
+        if (ret_str[i] == (str[i] & ~x))
+            c++;
+    }
+    if (c == sizeof(str) - 1){
+        test_ret_bitwise("test_toupper_f()", EXIT_SUCCESS);
+    }else{
+        test_ret_bitwise("test_toupper_f()", EXIT_FAILURE);
+    }
 };
