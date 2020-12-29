624a571adf6a6246c951e94b442ead027192dcb7
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-29 13:07:01 -0300

    Define ret_err and ret_scc

--- test/test.h
+++ test/test.h
@@ -17,3 +17,6 @@ char *test_toupper_f(char str[]);
 char *test_tolower_f(char str[]);
 char *test_lower_f(char str[]);
 char *test_upper_f(char str[]);
+
+#define ret_err(tname, ret) fprintf(stderr, "%s failed in file %s at line # %d \nreturn %d\n", tname, __FILE__, __LINE__, ret);
+#define ret_scc(tname, ret) fprintf(stdout, "%s was successful in file %s \nreturn %d\n", tname, __FILE__, ret);
