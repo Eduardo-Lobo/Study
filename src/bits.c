d0db4869a07707099aec72748355ac6333a9f499
Eduardo-Lobo <myedudev@protonmail.ch>
2020-12-30 09:13:49 -0300

    Replace type unsigned int

--- src/bits.c
+++ src/bits.c
@@ -1,46 +1,48 @@
 /*
- *
- * 
- * 
+ * Author: Eduardo-Lobo @2020
+ * Email : myedudev@protonmail.ch
  */
-/*                */
+
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
-/*                */
 #include "bits.h"
 
 
-unsigned int
-cnt_bits(unsigned int n)
-{
-    unsigned int c=0;
+uint16_t
+cnt_bits(uint16_t n){
+    uint8_t c = 0;
 
     while (n){
         c++;
         n >>= 1;
     }
-    if (c == CBITS){
-        return 1;
+
+    if (c == BITS){
+        return 0;
     }
     else
-        return 0;
+        return 1;
 };
 
 
-unsigned int 
-test_bit(unsigned int n, unsigned int p)
-{
-    unsigned int *y;
+uint16_t
+test_bit(uint16_t n, uint16_t p){
 
-    if ((cnt_bits(n)) && (range(p))){
+    uint8_t cnt, rng;
+    uint16_t *bin;
+
+    cnt = cnt_bits(n);
+    rng = range(p);
+
+    if (!(cnt && rng)){
+        bin = tobin(n);
         
-        y = tobin(n);
-        
-        if (y[p] == 1){
+        if (bin[p] == 1){
             return true;
         }
-        else if (y[p] == 0){
+        else if (bin[p] == 0){
             return false;
         }
     }else
@@ -48,33 +50,34 @@ test_bit(unsigned int n, unsigned int p)
 };
 
 
-unsigned int
-range(unsigned int n)
-{
-    if (n >= 0 || n <= 15)
-    {
-        return 1;
-    }
-    else
+uint16_t
+range(uint16_t n){
+
+    if (n >= 0 || n <= RANGE)
         return 0;
+    else
+        return 1;
 };
 
 
-unsigned int
-*tobin(unsigned int n)
-{
-    int i=0, c, k;
-    static unsigned int x[SIZE];
+uint16_t
+*tobin(uint16_t n){
 
-    if (cnt_bits(n)){
+    static uint16_t arr[SIZE];
+    int i = 0, d, r;
+    uint8_t cnt;
+
+    cnt = cnt_bits(n);
+
+    if (!(cnt)){
         
-        for (c=15; c >= 0; c--){
+        for (d = 15; d >= 0; d--){
              
-            k = n >> c;
-            x[i] = k & 1;
+            r = n >> d;
+            arr[i] = r & 1;
             i++;
         }
-        return x;
+        return arr;
     }else
         return 0;
 };
--- src/bits.h
+++ src/bits.h
@@ -1,19 +1,20 @@
 /*
- *
- * 
- * 
- */ 
+ * Author: Eduardo-Lobo @2020
+ * Email : myedudev@protonmail.ch
+ */
+
+#include <stdint.h>
 
 /* 
 Counts the number of one bits in a 16-bit integer value.
 */
-extern unsigned int cnt_bits(unsigned int number);
+extern uint16_t cnt_bits(uint16_t n);
 /* 
 Check if n have a range 0..15.
 The function should always return 1 if n holds a 
 value outside the range 0..15, otherwise 0. 
 */
-extern unsigned int range(unsigned int n);
+extern uint16_t range(uint16_t n);
 /* 
 This function requires two 16-bit integer parameters. 
 The first parameter is a 16-bit value to test; 
@@ -24,12 +25,11 @@ The function should return <b>true</> if the corresponding bit
 contains a 1, the function should return <b>false</b> if that 
 bit position contains a 0.
 */
-extern unsigned int test_bit(unsigned int n, unsigned int p);
+extern uint16_t test_bit(uint16_t n, uint16_t p);
 /* 
 Take a integer 16-bit and convert to binary.
 Return a array.
 */
-extern unsigned int *tobin(unsigned int n);
+extern uint16_t *tobin(uint16_t n);
 
-#define CBITS 15
-#define SIZE 16
+enum Bits {SIZE = 16, BITS = 16, RANGE = 16};
